22f2d8a029fbed00967947c4c663fcfff7eaf2f6
Mark-Dang <markd4@uci.edu>
2025-03-14 08:39:34 +0000

    Dijkstra methods finished

--- src/.ipynb_checkpoints/dijkstras-checkpoint.cpp
+++ src/.ipynb_checkpoints/dijkstras-checkpoint.cpp
@@ -6,14 +6,12 @@ struct Node{
     int weight;
 };
 
+//return longest distance to a vertex starting from G
 vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& previous) {
     //adjacency list: vector containing vector of edges
 
-    //the return is the distance to each node from the given:
-    //NOTE: WE CAN EDIT THE PARENT VECTOR
-    vector<int> result;
-    vector<bool> visited(G.numVertices-1, false);
-    vector<int> distances(G.numVertices-1, INF);
+    vector<bool> visited(G.numVertices, false);
+    vector<int> distances(G.numVertices, INF);
 
     auto compare = [](const Node& a, const Node& b) {return a.weight > b.weight;};
     priority_queue<Node, std::vector<Node>, decltype(compare)> pq(compare);
@@ -24,29 +22,49 @@ vector<int> dijkstra_shortest_path(const Graph& G, int source, vector<int>& prev
     while (!pq.empty()) {
         Node current = pq.top();
         pq.pop();
-        
-        int vertex = current.vertex;
-        result.push_back(vertex);
+        int curvertex = current.vertex;
 
-        if (visited[vertex] == true) continue;
+        if (visited[curvertex] == true) continue;
 
-        visited[vertex] = true;
-        for (auto n: G[vertex]) {
+        visited[curvertex] = true;
+        for (auto n: G[curvertex]) {
             int nvertex = n.dst;
             int nweight = n.weight;
 
-            if (!visited[nvertex] && distances[vertex] + nweight < distances[nvertex]){
-                distances[nvertex] = distances[vertex] + nweight;
+            if (!visited[nvertex] && distances[curvertex] + nweight < distances[nvertex]){
+                previous[nvertex] = curvertex;
+                distances[nvertex] = distances[curvertex] + nweight;
                 pq.push(Node(nvertex, distances[nvertex]));
             }
         }
     }
 
-    return result;
+    return distances;
 }
 
 
 
-//vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination);
+vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector<int>& previous, int destination) {
+    stack<int> vertices;
+    vertices.push(destination);
+    vector<int> path;
 
-//void print_path(const vector<int>& v, int total);
+    int curvertex = destination;
+    while (previous[curvertex] != -1) {
+        vertices.push(previous[curvertex]);
+        curvertex = previous[curvertex];
+    }
+
+    while (!vertices.empty()) {
+        path.push_back(vertices.top());
+        vertices.pop();
+    }
+
+    return path;
+}
+
+void print_path(const vector<int>& v, int total) {
+    for (auto vertex: v) std::cout << vertex << ' ';
+    std::cout << std::endl;
+    std::cout << "Total cost is " << total << std::endl;
+}
--- src/.ipynb_checkpoints/dijkstras_main-checkpoint.cpp
+++ src/.ipynb_checkpoints/dijkstras_main-checkpoint.cpp
@@ -1,5 +1,17 @@
 #include "dijkstras.h"
 
 int main() {
+    Graph G;
+    file_to_graph("src/small.txt", G);
+    vector<int> parent(G.numVertices, -1);
+    std::cout << "dijkstra starting" << std::endl;
+    vector<int> v = dijkstra_shortest_path(G, 0, parent);
+    for (auto e: v) std::cout << e << ' ';
+    std::cout << std::endl;
+
+    for (int i = 0; i < G.numVertices; ++i) {
+        vector<int> path = extract_shortest_path(v, parent, i);
+        print_path(path, v[i]);
+    }
     return 0;
 }
